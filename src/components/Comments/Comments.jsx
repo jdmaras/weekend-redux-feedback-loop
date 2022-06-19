418aa9551da176c6c6c2ab6858694706f014414e
John Maras <sportsballenthusiast@gmail.com>
2022-06-19 16:26:46 -0500

    comments now show up in review

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -6,20 +6,20 @@ function Comments(){
 
   const dispatch = useDispatch();
   const history = useHistory();
-  const [comments, setComments] = useState(0)
+  const [comments, setComments] = useState('')
 
   const handleSubmit = () => {
     dispatch({
       type: 'USER_COMMENTS',
       payload: comments
     })
+    history.push('/review')
   }
 
     return(
         <div>
           <h3>Any comments you want to leave?</h3>
-          <h5>Please put a number 1-5</h5>
-        <input required type="text" placeholder="Comments Here"></input>
+        <input required type="text" placeholder="Comments Here" value={comments} onChange={(e) => setComments(e.target.value)}></input>
         <button onClick={handleSubmit}>NEXT</button>
         </div>
     )
